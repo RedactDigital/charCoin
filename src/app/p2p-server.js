5c85720855534a54f018cf99a265e989ed13bc65
patrick <patrick.d.rizzardi@gmail.com>
2022-02-23 18:12:05 -0500

    add log

--- src/app/p2p-server.js
+++ src/app/p2p-server.js
@@ -57,7 +57,7 @@ class P2pserver {
     socket.on('message', message => {
       const data = JSON.parse(message);
 
-      if ((data.type == 'block' && data.validators[0].address) != this.Wallet.getPublicKey()) return;
+      if (data.type == 'block' && data.validators[0].address != this.Wallet.getPublicKey()) return;
 
       log.info(`Received data from peer: ${data.type}`);
       switch (data.type) {
