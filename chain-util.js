634073e94d97ff9484702422dbbeca6fbd18253a
patrick <patrick.d.rizzardi@gmail.com>
2022-02-22 19:12:50 -0500

    correct get leader error

--- chain-util.js
+++ chain-util.js
@@ -1,9 +1,9 @@
-const EDDSA = require("elliptic").eddsa;
-const eddsa = new EDDSA("ed25519");
+const EDDSA = require('elliptic').eddsa;
+const eddsa = new EDDSA('ed25519');
 
-const uuidV1 = require("uuid/v1");
+const uuidV1 = require('uuid/v1');
 
-const SHA256 = require("crypto-js/sha256");
+const SHA256 = require('crypto-js/sha256');
 
 class ChainUtil {
   static genKeyPair(secret) {
@@ -15,6 +15,7 @@ class ChainUtil {
   }
 
   static hash(data) {
+    console.log(data);
     return SHA256(JSON.stringify(data)).toString();
   }
 
