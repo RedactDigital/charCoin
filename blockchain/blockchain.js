6489c69fbb534d8b17a67384ad09b91f5207585e
patrick <patrick.d.rizzardi@gmail.com>
2022-02-22 20:02:06 -0500

    correct get leader error

--- blockchain/blockchain.js
+++ blockchain/blockchain.js
@@ -100,7 +100,7 @@ class Blockchain {
     const lastBlock = this.chain[this.chain.length - 1];
     if (block.lastHash === lastBlock.hash && block.hash === blockHash(block) && verifyBlock(block)) {
       for (let i = 0; i < block.validators.length; i++) {
-        console.log(this.findValidator([i]).address);
+        console.log(this.findValidator([i]));
         if (block.validators[i] != this.findValidator([i])) return false;
       }
       this.addBlock(block);
