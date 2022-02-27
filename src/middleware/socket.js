f3f78f7f84e5bcf5e47bd17814c6a00467c339fa
patrick <patrick.d.rizzardi@gmail.com>
2022-02-26 22:29:55 -0500

    sync

--- src/middleware/socket.js
+++ src/middleware/socket.js
@@ -2,157 +2,78 @@ require('dotenv').config({ silent: true });
 require('../globals');
 
 const { io } = require('socket.io-client');
-const { getValidator } = require('../blockchain/validators');
-const { createBlock } = require('../blockchain/block');
-
-const P2P_PORT = process.env.P2P_PORT || 5000;
 
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
-const MESSAGE_TYPE = {
-  chain: 'chain',
-  block: 'block',
-  transaction: 'transaction',
+const sockets = [];
+
+const connectToPeers = () => {
+  peers.forEach(peer => {
+    const socket = io(peer);
+    socket.on('connect', () => connectSocket(socket));
+  });
 };
 
-class P2pserver {
-  constructor(blockchain, transactionPool, Wallet) {
-    this.blockchain = blockchain;
-    this.sockets = [];
-    this.transactionPool = transactionPool;
-    this.Wallet = Wallet;
+const connectSocket = socket => {
+  sockets.push(socket);
+  log.info('Socket connected');
+};
 
-    this.connectToPeers();
-  }
+const sendChain = socket => {
+  socket.emit(
+    'message',
+    JSON.stringify({
+      type: 'chain',
+      chain: blockchain.chain,
+    })
+  );
+};
 
-  listen() {
-    // Connection listener
-    server.on('connection', socket => {
-      socket.isAlive = true;
-      this.connectSocket(socket);
-    });
+const syncChain = () => {
+  sockets.forEach(socket => {
+    sendChain(socket);
+  });
+};
 
-    // Creates a new socket connection for each peer
-    // this.connectToPeers();
-    log.info(`Listening for peer to peer connection on port : ${P2P_PORT}`);
-  }
+const broadcastTransaction = transaction => {
+  sockets.forEach(socket => {
+    sendTransaction(socket, transaction);
+  });
+};
 
-  connectSocket(socket) {
-    this.sockets.push(socket);
-    log.info('Socket connected');
-    this.messageHandler(socket);
-    this.closeConnectionHandler(socket);
-    this.sendChain(socket);
-  }
+const sendTransaction = (socket, transaction) => {
+  socket.emit(
+    'message',
+    JSON.stringify({
+      type: 'transaction',
+      transaction: transaction,
+    })
+  );
+};
 
-  connectToPeers() {
-    peers.forEach(peer => {
-      const socket = io(peer);
-      socket.on('open', () => this.connectSocket(socket));
-    });
-  }
+const broadcastBlock = block => {
+  sockets.forEach(socket => {
+    sendBlock(socket, block);
+  });
+};
 
-  messageHandler(socket) {
-    socket.on('message', message => {
-      const data = JSON.parse(message);
+const sendBlock = (socket, block) => {
+  socket.emit(
+    'message',
+    JSON.stringify({
+      type: 'block',
+      block: block,
+    })
+  );
+};
 
-      log.info(`Received data from peer: ${data.type}`);
-      switch (data.type) {
-        case 'chain':
-          this.blockchain.replaceChain(data.chain);
-          break;
-
-        case 'transaction':
-          if (!data.transaction) break;
-          if (!this.transactionPool.transactionExists(data.transaction)) {
-            this.transactionPool.addTransaction(data.transaction);
-            this.broadcastTransaction(data.transaction);
-          }
-          if (this.transactionPool.thresholdReached()) {
-            // Verify the wallet exists on the blockchain as a validator
-            // If the validator doesn't exist, the next validator will be the next in line
-
-            const validatorExists = getValidator(this.Wallet.getPublicKey());
-
-            if (validatorExists) {
-              log.info('Creating block');
-              const block = createBlock(
-                this.blockchain.chain[this.blockchain.chain.length - 1],
-                this.transactionPool.transactions,
-                this.Wallet
-              );
-              this.broadcastBlock(block);
-            }
-          }
-
-          break;
-
-        case 'block':
-          if (!data.block) break;
-          if (!data.block.validators) break;
-          if (this.blockchain.isValidBlock(data.block)) {
-            this.blockchain.addBlockToChain(data.block);
-            this.blockchain.executeTransactions(data.block);
-
-            this.transactionPool.clear();
-          }
-
-          break;
-      }
-    });
-  }
-
-  closeConnectionHandler(socket) {
-    socket.on('close', () => {
-      this.sockets = this.sockets.filter(s => s.readyState === WebSocket.OPEN);
-      log.info('Socket disconnected');
-    });
-  }
-
-  sendChain(socket) {
-    socket.send(
-      JSON.stringify({
-        type: MESSAGE_TYPE.chain,
-        chain: this.blockchain.chain,
-      })
-    );
-  }
-
-  syncChain() {
-    this.sockets.forEach(socket => {
-      this.sendChain(socket);
-    });
-  }
-
-  broadcastTransaction(transaction) {
-    this.sockets.forEach(socket => {
-      this.sendTransaction(socket, transaction);
-    });
-  }
-
-  sendTransaction(socket, transaction) {
-    socket.send(
-      JSON.stringify({
-        type: MESSAGE_TYPE.transaction,
-        transaction: transaction,
-      })
-    );
-  }
-
-  broadcastBlock(block) {
-    this.sockets.forEach(socket => {
-      this.sendBlock(socket, block);
-    });
-  }
-
-  sendBlock(socket, block) {
-    socket.send(
-      JSON.stringify({
-        type: MESSAGE_TYPE.block,
-        block: block,
-      })
-    );
-  }
-}
-
-module.exports = P2pserver;
+module.exports = {
+  connectToPeers,
+  connectSocket,
+  sendChain,
+  syncChain,
+  broadcastTransaction,
+  sendTransaction,
+  broadcastBlock,
+  sendBlock,
+};
