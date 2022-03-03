c86bcb44c051d70817c2e29fd1c18dc0970913a5
patrick <patrick.d.rizzardi@gmail.com>
2022-03-03 16:55:09 -0500

    sync

--- src/wallet/transaction.js
+++ src/wallet/transaction.js
@@ -1,51 +1,55 @@
 const ChainUtil = require('../chain-util');
 
-class Transaction {
-  constructor() {
-    this.id = ChainUtil.id();
-    this.type = null;
-    this.input = null;
-    this.output = null;
-  }
+// class Transaction {
+//   constructor() {
+//     this.id = ChainUtil.id();
+//     this.type = null;
+//     this.input = null;
+//     this.output = null;
+//   }
 
-  static newTransaction(senderWallet, to, amount, type) {
-    if (Number(amount) + Number(TRANSACTION_FEE) > senderWallet.balance) {
-      log.info(`Amount : ${amount + TRANSACTION_FEE} exceeds the balance of ${senderWallet.balance}`);
-      return;
-    }
+//   // static newTransaction(senderWallet, to, amount, type) {
+//   //   if (Number(amount) + Number(TRANSACTION_FEE) > senderWallet.balance) {
+//   //     log.info(`Amount : ${amount + TRANSACTION_FEE} exceeds the balance of ${senderWallet.balance}`);
+//   //     return;
+//   //   }
 
-    return Transaction.generateTransaction(senderWallet, to, amount, type);
-  }
+//   //   const transaction = new this();
+//   //   transaction.type = type;
+//   //   transaction.output = {
+//   //     to: to,
+//   //     amount: amount - TRANSACTION_FEE,
+//   //     fee: TRANSACTION_FEE,
+//   //   };
+//   //   Transaction.signTransaction(transaction, senderWallet);
+//   //   return transaction;
+//   // }
 
-  static generateTransaction(senderWallet, to, amount, type) {
-    const transaction = new this();
-    transaction.type = type;
-    transaction.output = {
-      to: to,
-      amount: amount - TRANSACTION_FEE,
-      fee: TRANSACTION_FEE,
-    };
-    Transaction.signTransaction(transaction, senderWallet);
-    return transaction;
-  }
+//   // TODO -- validate transactions method
 
-  // TODO -- validate transactions method
+//   // static signTransaction(transaction, senderWallet) {
+//   //   transaction.input = {
+//   //     timestamp: Date.now(),
+//   //     from: senderWallet.publicKey,
+//   //     signature: senderWallet.sign(ChainUtil.hash(transaction.output)),
+//   //   };
+//   // }
 
-  static signTransaction(transaction, senderWallet) {
-    transaction.input = {
-      timestamp: Date.now(),
-      from: senderWallet.publicKey,
-      signature: senderWallet.sign(ChainUtil.hash(transaction.output)),
-    };
-  }
+//   // static verifyTransaction(transaction) {
+//   //   return ChainUtil.verifySignature(
+//   //     transaction.input.from,
+//   //     transaction.input.signature,
+//   //     ChainUtil.hash(transaction.output)
+//   //   );
+//   // }
+// }
 
-  static verifyTransaction(transaction) {
+module.exports = {
+  verifyTransaction: transaction => {
     return ChainUtil.verifySignature(
       transaction.input.from,
       transaction.input.signature,
       ChainUtil.hash(transaction.output)
     );
-  }
-}
-
-module.exports = Transaction;
+  },
+};
--- src/wallet/wallet.js
+++ src/wallet/wallet.js
@@ -21,11 +21,28 @@ class Wallet {
   createTransaction(to, amount, type, blockchain, transactionPool) {
     this.balance = blockchain.getBalance(this.publicKey);
 
-    if (amount > this.balance) {
-      log.info(`Amount: ${amount} exceeds the current balance: ${this.balance}`);
+    log.error(+amount + +TRANSACTION_FEE);
+
+    if (+amount + +TRANSACTION_FEE > this.balance) {
+      log.info(`Amount : ${amount + TRANSACTION_FEE} exceeds the balance of ${this.balance}`);
       return;
     }
-    const transaction = Transaction.newTransaction(this, to, amount, type);
+
+    const transaction = {
+      id: ChainUtil.id(),
+      type,
+      input: {
+        timestamp: Date.now(),
+        from: this.publicKey,
+        signature: this.sign(ChainUtil.hash(transaction.output)),
+      },
+      output: {
+        to: to,
+        amount: amount - TRANSACTION_FEE,
+        fee: TRANSACTION_FEE,
+      },
+    };
+
     transactionPool.addTransaction(transaction);
     return transaction;
   }
