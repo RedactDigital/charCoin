e92dc3c07d0b5e882881b70c35f3db8aa52f5504
patrick <patrick.d.rizzardi@gmail.com>
2022-03-04 00:20:02 -0500

    sync

--- src/blockchain/validators.js
+++ src/blockchain/validators.js
@@ -35,7 +35,7 @@ module.exports = {
     //   }
     // }
     // return chosenValidator;
-    let chosenValidator = validators[0];
+    let chosenValidator = undefined;
     for (let i = 0; i < validators.length; i++) {
       if (accounts.getBalance(validators[i].address) > accounts.getBalance(chosenValidator.address)) {
         chosenValidator = validators[i];
