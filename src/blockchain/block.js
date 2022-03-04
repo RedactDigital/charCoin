5e0e5bd4aa91b30e58a6c49d378e726dd6344b5f
patrick <patrick.d.rizzardi@gmail.com>
2022-03-03 20:12:13 -0500

    sync

--- src/blockchain/block.js
+++ src/blockchain/block.js
@@ -2,6 +2,7 @@ const ChainUtil = require('../chain-util');
 
 module.exports = {
   genesisBlock: { timestamp: 0, lastHash: '-----', hash: 'genesis', data: [], validators: [] },
+
   createBlock: (lastBlock, data, wallet) => {
     const timestamp = Date.now();
     const lastHash = lastBlock.hash;
