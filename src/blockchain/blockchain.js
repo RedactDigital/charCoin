106012f8ecbbed1674d0c0ef5289c43e04d80393
patrick <patrick.d.rizzardi@gmail.com>
2022-03-04 13:05:05 -0500

    sync

--- src/blockchain/blockchain.js
+++ src/blockchain/blockchain.js
@@ -14,21 +14,21 @@ const blockchain = {
   accounts: new Account(),
 };
 
-const isValidChain = chain => {
-  // TODO - put better checks in
-  // Check if the genesis block is valid
-  if (JSON.stringify(chain[0]) !== JSON.stringify(genesisBlock)) return false;
+// const isValidChain = blocks => {
+//   // TODO - put better checks in
+//   // Check if the genesis block is valid
+//   if (JSON.stringify(chain[0]) !== JSON.stringify(genesisBlock)) return false;
 
-  // Check if the chain is valid
-  for (let i = 1; i < chain.length; i++) {
-    const block = chain[i];
-    const lastBlock = chain[i - 1];
+//   // Check if the chain is valid
+//   for (let i = 1; i < chain.length; i++) {
+//     const block = chain[i];
+//     const lastBlock = chain[i - 1];
 
-    if (chain[i].lastHash !== lastBlock.hash || block.hash !== blockHash(block)) return false;
-  }
+//     if (chain[i].lastHash !== lastBlock.hash || block.hash !== blockHash(block)) return false;
+//   }
 
-  return true;
-};
+//   return true;
+// };
 
 module.exports = {
   blockchain,
@@ -40,9 +40,8 @@ module.exports = {
   },
 
   replaceChain: newChain => {
-    console.log(newChain);
     // Check if the chain is valid
-    if (!isValidChain(newChain)) return;
+    // if (!isValidChain(newChain)) return;
 
     // Check if the chain is longer than the current chain
     if (newChain.length <= this.chain.length) return;
