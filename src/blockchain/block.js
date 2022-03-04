f2ca25cebb6f9829134e4c33fb548d8a488c3433
patrick <patrick.d.rizzardi@gmail.com>
2022-03-04 12:59:25 -0500

    sync

--- src/blockchain/block.js
+++ src/blockchain/block.js
@@ -26,7 +26,7 @@ module.exports = {
     return ChainUtil.hash(`${timestamp}${lastHash}${transactions}`);
   },
 
-  verifyBlock: (currentBlock, previousBlock) => {
+  verifyBlock: block => {
     const { timestamp, lastHash, transactions, leader } = block;
 
     // const lastBlock = blockchain.getLastBlock();
